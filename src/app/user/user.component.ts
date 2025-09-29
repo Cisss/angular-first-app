5f8ffbb5f934848e63874253249215ceb9bf48ee
Cisss <franciscavanderwant@gmail.com>
2025-09-29 09:08:34 +0200

    All functionalities, show, add and delete tasks per user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,37 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-user';
+import { Component, computed, signal, Input, input, Output, output, EventEmitter } from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  constructor(){
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+  
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
   }
-  id = 0;
-  selectedUser = signal(DUMMY_USERS[this.id]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   onSelectUser() {
-    if(this.id < DUMMY_USERS.length - 1) {
-      this.id++;
-      this.selectedUser.set(DUMMY_USERS[this.id]);
-      console.log(this.id);
-    } else {
-      this.id = 0;
-      this.selectedUser.set(DUMMY_USERS[this.id]);
-    }
+    this.select.emit(this.user.id);
   }
+
+  // signal version
+    // select = output<string>();
+    // avatar = input.required<string>();
+    // name = input.required<string>();
+
+    // imagePath = computed(() => {
+    //   return 'assets/users/' + this.avatar();
+    // });
+
+    // onSelectUser() {
+    //   this.select.emit(this.id);
+    // }
 }
